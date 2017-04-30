25a68929d7cdaf6c7a87ea4cdd3130daae5dbbf6
researcher1 <birkan.kolcu@ozu.edu.tr>
2017-04-30 20:54:17 +0000

    package.json added

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var app = express();
 
 var allowCrossOrigin = require('./allowCrossOriginMiddleware');
 
-const websiteToConnect = "https://tr.wikipedia.org";
+const turkishWiki = "https://tr.wikipedia.org";
+const enWiki = "https://en.wikipedia.org";
 
-app.get('/get', function (req, res) {
+app.get('/en', function (req, res) {
 	var requestedUrl = req.param('page');
-	console.log("requestedUrl: "+requestedUrl);
-	console.log("new stuff");
-	request.get(websiteToConnect).pipe(res);
+	request.get(enWiki).pipe(res);
 });
 
 function myMiddleware (req, res, next) {
@@ -18,7 +17,7 @@ function myMiddleware (req, res, next) {
 	   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 		console.log("fullUrl: "+fullUrl);
 		console.log("originalUrl: "+req.originalUrl);
-		request.get(websiteToConnect+req.originalUrl).pipe(res);
+		request.get(turkishWiki+req.originalUrl).pipe(res);
    } else {
 	   next();
    }
