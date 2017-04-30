91b31f97ab6f417dc2e98ae0a1059f3a4a758de1
researcher1 <birkan.kolcu@ozu.edu.tr>
2017-04-30 20:32:23 +0000

    cleaned uncessary console.log

--- app.js
+++ app.js
@@ -4,14 +4,13 @@ var app = express();
 
 var allowCrossOrigin = require('./allowCrossOriginMiddleware');
 
-var justTest = "https://tr.wikipedia.org";
+const websiteToConnect = "https://tr.wikipedia.org";
 
 app.get('/get', function (req, res) {
 	var requestedUrl = req.param('page');
 	console.log("requestedUrl: "+requestedUrl);
 	console.log("new stuff");
-	//request.get(requestedUrl).pipe(res);
-	request.get(justTest).pipe(res);
+	request.get(websiteToConnect).pipe(res);
 });
 
 function myMiddleware (req, res, next) {
@@ -19,7 +18,7 @@ function myMiddleware (req, res, next) {
 	   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 		console.log("fullUrl: "+fullUrl);
 		console.log("originalUrl: "+req.originalUrl);
-		request.get(justTest+req.originalUrl).pipe(res);
+		request.get(websiteToConnect+req.originalUrl).pipe(res);
    } else {
 	   next();
    }
