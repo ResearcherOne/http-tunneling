ffef977ee37eb0504faf9a447c8cfe46bbd2533f
researcher1 <birkan.kolcu@ozu.edu.tr>
2017-05-01 07:09:03 +0000

    removed page param usage

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ var allowCrossOrigin = require('./allowCrossOriginMiddleware');
 const turkishWikipedia = "https://tr.wikipedia.org";
 const port = process.env.PORT || 8010;
 
-app.get('/get', function (req, res) {
-	var requestedUrl = req.param('page');
-	request.get(turkishWikipedia).pipe(res);
-});
-
 function myMiddleware (req, res, next) {
    if (req.method === 'GET') {
 	   var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
@@ -23,7 +18,7 @@ function myMiddleware (req, res, next) {
    }
 }
 
-app.use(myMiddleware)
+app.use(myMiddleware);
 
 app.listen(port, function () {
 	console.log('Example app listening on port '+port+'!');
